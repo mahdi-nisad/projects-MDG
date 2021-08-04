b8e423fda9280d04a1c044640aa86fac3d2c91eb
mahdi-nisad <61220593+mahdi-nisad@users.noreply.github.com>
2021-08-04 07:57:18 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+//communication
+var menubtn = document.getElementById("menubtn");
+var sidenav = document.getElementById("side");
+
+side.style.right ="-250px";
+menubtn.onclick = function() { 
+ if(side.style.right == "-250px" ){
+     side.style.right = "0px";
+
+ }
+ else{
+     side.style.right = "-250px";
+ }
+};
+
+
+
+
+
+
+
+
+
+
